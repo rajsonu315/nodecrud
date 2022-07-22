5886b100b666475ca136e46c395f203ce8d7640b
unknown <rs2420315@gmail.com>
2022-07-22 13:04:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import connectDB from "./db/connectdb.js";
+import {join} from 'path'
+import web from "./routes/web.js";
+const app = express()
+const port = process.env.PORT || '3000'
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://sonumonu:sonumonu@cluster0.ve68i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// Database Connection
+connectDB(DATABASE_URL);
+
+app.use(express.urlencoded({extended:false}))
+
+// Static Files
+app.use('/student',express.static(join(process.cwd(), "public")))
+app.use('/student/edit',express.static(join(process.cwd(), "public")))
+
+// Set Template Engine
+app.set("view engine", "ejs");
+
+// Load Routes
+app.use("/student", web);
+
+
+app.listen(port, () => {
+ console.log(`Server listening at http://localhost:${port}`)
+})
